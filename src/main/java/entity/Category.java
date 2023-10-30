751b445dde00fd202cf37c7fa56aa1f68e18441b
leek94 <khwan929@naver.com>
2023-10-30 11:12:30 +0900

    feat Category 생성

---
+++ src/main/java/entity/Category.java
@@ -0,0 +1,30 @@
+package entity;
+
+public class Category {
+    private String siteDepth1;
+    private String siteDepth2;
+    private String siteLink;
+
+    public Category(String siteDepth1, String siteDepth2, String siteLink) {
+        this.siteDepth1 = siteDepth1;
+        this.siteDepth2 = siteDepth2;
+        this.siteLink = siteLink;
+    }
+
+    public String getSiteDepth1() {
+        return siteDepth1;
+    }
+
+    public String getSiteDepth2() {
+        return siteDepth2;
+    }
+
+    public String getSiteLink() {
+        return siteLink;
+    }
+
+    @Override
+    public String toString() {
+        return "siteDepth1: " + siteDepth1 + ", siteDepth2: " + siteDepth2 + ", siteLink: " + siteLink;
+    }
+}
