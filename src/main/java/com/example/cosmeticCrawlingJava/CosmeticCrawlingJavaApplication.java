1ecf1363c1252274288cb0e60fc392547c107304
leek94 <khwan929@naver.com>
2023-10-30 10:45:17 +0900

    create 초기 파일 생성

---
+++ src/main/java/com/example/cosmeticCrawlingJava/CosmeticCrawlingJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.cosmeticCrawlingJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmeticCrawlingJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmeticCrawlingJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cosmeticCrawlingJava/CosmeticCrawlingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cosmeticCrawlingJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmeticCrawlingJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
