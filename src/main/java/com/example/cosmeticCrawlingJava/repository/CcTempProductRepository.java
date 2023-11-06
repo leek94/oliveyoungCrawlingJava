b2701acec14036a7ae4df9a98e7c4c06ce12f38d
leek94 <khwan929@naver.com>
2023-11-06 10:49:14 +0900

    feat: CcTempProductReository 생성

---
+++ src/main/java/com/example/cosmeticCrawlingJava/repository/CcTempProductRepository.java
@@ -0,0 +1,7 @@
+package com.example.cosmeticCrawlingJava.repository;
+
+import entity.CcTempProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CcTempProductRepository extends JpaRepository<CcTempProduct, Long> {
+}
